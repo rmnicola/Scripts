2ddd004c106ff875d58bfba1e25b7e87e269f9d0
Rodrigo Nicola <rnicola@maua.br>
2022-01-01 15:24:17 -0300

    Fix: remove redundant steps
    Includes checks and options so that no redundant work is done

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -2,22 +2,39 @@
 
 # Configures dnf
 echo "Configuring dnf..."
-DNF_CONF_PATH="/etc/dnf/dnf.conf"
+
+declare DNF_CONF_PATH="/etc/dnf/dnf.conf"
 declare -a CONF_ARRAY=("fastest_mirror=True" "max_parallel_downloads=10")
+declare -i DNF_CHANGES=0
+
 # Loop every configuration and checks if it already exists
 for CONF in ${CONF_ARRAY[@]}; do
-	if ! grep -Fxq "$CONF" $DNF_CONF_PATH; then 
+	if ! grep -Fxq $CONF $DNF_CONF_PATH ; then 
 		# If it does not exist, add it
 		sudo sed -i "/^[main]/a $CONF" $DNF_CONF_PATH 
 		echo "Added option $CONF!"
+		let "DNF_CHANGES+=1"
 	else
 		echo "Option $CONF has already been added"
 	fi
 done
-# Updates dnf config
-echo "Cleaning up..."
-sudo dnf clean all
+
+# Updates dnf config if there were changes
+if [ $DNF_CHANGES -gt 0 ] ; then
+	echo "Cleaning up..."
+	sudo dnf clean all
+else
+	echo "No changes were made, moving on..."
+fi
 
 # Updates system packages
-echo "Updating entire system. This may take a few minutes..."
-sudo dnf -y update
+read -p "Update system (this may take a few minutes)? [y/N] " UPDATE_OPTION
+if [ "${UPDATE_OPTION,,}" = "y" ] ; then
+	sudo dnf -y update
+fi
+
+# Download and configure zsh
+read -p "Do you wish to use ZSH instead of BASH? [y/N] " ZSH_OPTION
+if [ "${ZSH_OPTION,,}" = "y" ] ; then
+	sudo dnf -y install zsh
+fi
