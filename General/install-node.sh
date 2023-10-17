bfd70d730d0700d3466cc313549a9e550c5e2018
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2023-10-17 15:15:24 -0300

    Update install node package

--- General/install-node.sh
+++ General/install-node.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
+#
+
+if ! command -v cargo &> /dev/null; then
+  echo "Rust no installed. Installing..."
+  install-rust
+else
+  echo "Rust already installed..."
+fi
 
 # Check if 'fnm' command is available
 if ! command -v fnm &> /dev/null; then
-    echo "fnm is not installed. Installing now..."
-    curl -fsSL https://fnm.vercel.app/install | bash
-    
-    # Detect the shell in use
-    current_shell=$(basename $SHELL)
-    if [ "$current_shell" == "zsh" ]; then
-        # Check for the existence of the Dotfiles/zsh directory
-        if [ -d "$HOME/Dotfiles/zsh" ]; then
-            source "$HOME/Dotfiles/zsh/zshrc"
-        else
-            # Fallback to the standard zshrc location
-            if [ -n "$ZDOTDIR" ]; then
-                source "$ZDOTDIR/.zshrc"
-            else
-                source "$HOME/.zshrc"
-            fi
-        fi
-    elif [ "$current_shell" == "bash" ]; then
-        source ~/.bashrc
-    else
-        echo "Unsupported shell detected. Please manually source your shell's configuration file to use 'fnm'."
-    fi
+  echo "fnm is not installed. Installing now..."
+  cargo install fnm
 else
-    echo "fnm is already installed."
+  echo "fnm is already installed."
 fi
 
 # Use 'fnm' to install latest version of Node.js
-fnm install --lts
+fnm install --latest
 echo "Node.js has been installed via fnm."
 node --version
