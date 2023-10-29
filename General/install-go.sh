b312ab1e67d964a562c3524a05d4245f05ef1012
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2023-10-29 20:36:46 -0300

    Add go install script

---
+++ General/install-go.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# codenoid
+# https://gist.github.com/codenoid/4806365032bb4ed62f381d8a76ddb8e6
+sudo apt install -y curl
+printf "Checking latest Go version...\n";
+LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)";
+LATEST_GO_DOWNLOAD_URL="https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
+
+printf "cd to home ($USER) directory \n"
+cd $HOME
+
+printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n";
+curl -OJ -L --progress-bar $LATEST_GO_DOWNLOAD_URL
+
+printf "Extracting file...\n"
+tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
+rm ${LATEST_GO_VERSION}.linux-amd64.tar.gz
+
+export GOROOT="$HOME/go"
+export GOPATH="$HOME/go/packages"
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR bashrc/zshrc  ⚠️
+
+export GOROOT="$HOME/go"
+export GOPATH="$HOME/go/packages"
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+\n'
+
+printf "You are ready to Go!";
+go version
