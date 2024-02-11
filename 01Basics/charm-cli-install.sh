670d5abbfca69d68a67d5c913763959e9576a97f
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2024-02-11 20:35:51 -0300

    Clean up charm install script

--- 01Basics/charm-cli-install.sh
+++ 01Basics/charm-cli-install.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
+if [[ ! -x $(which go) ]]; then
+  echo "Go is not installed. You need Go to proceed."
+  echo "Try running go-install."
+  exit 1
+fi
+
 if [[ -x $(which gum) ]]; then
   echo "Gum is already installed"
 else
   echo "Installing Gum..."
+  sleep 2
   go install github.com/charmbracelet/gum@latest
 fi
 
@@ -11,5 +18,6 @@ if [[ -x $(which vhs) ]]; then
   echo "VHS is already installed"
 else
   echo "Installing VHS..."
+  sleep 2
   go install github.com/charmbracelet/vhs@latest
 fi
